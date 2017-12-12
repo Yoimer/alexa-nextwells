3fa112176bc4e828f4b188caea565a84ee37392c
Yoimer <ydrf19@gmail.com>
2017-12-12 12:03:39 -0400

    rolled back to turn on or off by voice commands

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from flask import Flask, render_template
 
 from flask_ask import Ask, statement, question, session
 
-from flask import Flask, abort, request 
-import json
-
 ######################################
 from flask import Flask
 
@@ -28,16 +25,6 @@ ask = Ask(app, "/")
 
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
-@app.route('/nodemcu') 
-def nodemcu():
-    lastvalue = request.args.get('lastvalue')
-    if lastvalue == 'ON' or lastvalue == 'OFF':
-        return lastvalue
-    else:
-        return 'OK'
-	# here we want to get the value of user (i.e. ?user=some-value)
-    #lastvalue = request.args.get('lastvalue')
-    #return lastvalue
 
 @ask.launch
 
@@ -52,18 +39,21 @@ def launch_app():
 
 def turn_on():
 
-    if lastvalue == 'ON':
-        turn_on_msg = "System is already ON. No action will be taken"
-        return statement(turn_on_msg)
-    else:
-        sess = requests.Session()
-        # sent ON to Nodemcu by sendind 7 to db on castillolk
-        url = 'http://castillolk.com.ve/proyectos/sms/alexa.php?sw=7'
-        data = sess.get(url)
-        print data.content
-        print "next line is the statement"
-        turn_on_msg = "Turning system ON... It might take a few seconds, please wait."
-        return statement(turn_on_msg)
+    sess = requests.Session()
+
+    # sent ON to Nodemcu by sendind 7 to db on castillolk
+    url = 'http://castillolk.com.ve/proyectos/sms/alexa.php?sw=7'
+     
+    data = sess.get(url)
+     
+    print data.content
+     
+    print "next line is the statement"
+
+    turn_on_msg = "Turning system ON... It might take a few seconds, please wait."
+     
+    return statement(turn_on_msg)
+
 
 @ask.intent("TurnOffIntent")
 
@@ -87,4 +77,4 @@ def turn_off():
 
 if __name__ == '__main__':
 
-    app.run(debug=True,  host='0.0.0.0')
+    app.run(debug=True)
